cfebf5f66f9a45cb4252ece149ccc7137e594bf3
Charalampos Karypidis <xaralaboskaripidis@gmail.com>
2016-01-26 20:59:43 +0100

    Add bootstrap libary to the project

--- reactapp/index.js
+++ reactapp/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+require('bootstrap');
+
 import App from './components/app.jsx';
 
 const reactAppNode = document.getElementById('react-app');
